19cd9666c8353629b8698b7240120b6f1c09f8d9
Charity <umercy001@gmail.com>
2021-08-27 14:23:34 +0100

    updated files

--- TTodo/settings.py
+++ TTodo/settings.py
@@ -9,24 +9,23 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
-import django_heroku
-import dj_database_url
 import os
-from decouple import config
+from pathlib import Path
+
+import django_heroku
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY ='django-insecure-4(+*sclse-7_%77!fsy@j9ct6%)xt6)3267#a^ivvz$y%n(f!%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG= config('DEBUG', cast=bool, default =True)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -133,3 +132,5 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
